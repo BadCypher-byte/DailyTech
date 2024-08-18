0764c789a97d6e8cfa60dfbc743f11947a9a7494
BadCypher-byte <aniketv5730@gmail.com>
2024-08-18 12:45:45 +0530

    Intial version

--- src/main/java/com/example/dailytech/DailyTechApplication.java
+++ src/main/java/com/example/dailytech/DailyTechApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DailyTechApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DailyTechApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DailyTechApplication.class, args);
+    }
 
 }
